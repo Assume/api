e0873b20c4f8f3ca5547c7411aeee552d506bf63
Adam <one@pbot.org>
2017-03-08 14:06:09 -0500

    imports

--- scriptapi/generic/Walking07.java
+++ scriptapi/generic/Walking07.java
@@ -1,44 +1,17 @@
 package scripts.api.scriptapi.generic;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.tribot.api.General;
 import org.tribot.api.types.generic.Condition;
-import org.tribot.api2007.Camera;
 import org.tribot.api2007.Game;
-import org.tribot.api2007.Player;
 import org.tribot.api2007.Walking;
 import org.tribot.api2007.WebWalking;
 import org.tribot.api2007.types.RSTile;
 
 import scripts.api.scriptapi.antiban.AntiBan;
-import scripts.starfox.api2007.banking.Bank07;
-import scripts.starfox.api2007.walking.Map07;
-import scripts.starfox.manager.moveSet.ActionSet;
 
 /**
  * @author Nolan
  */
 public class Walking07 {
-
-    private static ArrayList<RSTile> current_path;
-    private static final Object path_lock;
-
-    static {
-        path_lock = new Object();
-    }
-
-    public static ArrayList<RSTile> getCurrentPath() {
-        if (current_path == null) {
-            return null;
-        }
-        synchronized (path_lock) {
-            return (ArrayList<RSTile>) current_path.clone();
-        }
-    }
-
-
   
     /**
      * Walks in a straight line towards the destination.
@@ -90,56 +63,7 @@ public class Walking07 {
     }
 
     /**
-     * Attempts to screen-walk to a random reachable nearby tile.
-     *
-     * @return True if the walking was successful, false otherwise.
-     */
-    public static boolean walkNearbyTile() {
-        final RSTile pos = Player07.getPosition();
-        if (pos != null) {
-            RSTile tile = null;
-            while (tile == null || !Map07.isStandardBlocked(tile)) {
-                tile = pos.translate(General.random(-5, 5), General.random(-5, 5));
-            }
-            return Walking.clickTileMS(tile, "Walk here");
-        }
-        return false;
-    }
 
-    /**
-     * Attempts to walk directly to a target tile using the screen.
-     *
-     * This method will attempt to reach the EXACT tile that you pass in for 7.5 seconds. If the tile that is passed in is not pathable, this method returns false.
-     *
-     * @param target The target tile.
-     * @return True if the target was reached, false otherwise.
-     */
-    public static boolean walkDirect(RSTile target) {
-        RSTile destination = Game.getDestination();
-        if (destination != null && destination.equals(target)) {
-            return true;
-        }
-        final RSTile pos = Player07.getPosition();
-        if (target != null && pos != null) {
-            if (Map07.isStandardBlocked(target)) {
-                return false;
-            }
-            if (!target.isOnScreen()) {
-                Camera.turnToTile(target);
-                Client.sleep(150, 300);
-            }
-            Timer timer = new Timer(7500);
-            timer.start();
-            while (!timer.timedOut() && !Player07.getPosition().equals(target)) {
-                Client.sleep(25, 200);
-                AntiBan.moveCamera();
-                if (!Player.isMoving()) {
-                    Walking.clickTileMS(target, "Walk here");
-                }
-            }
-        }
-        return false;
-    }
 
     /**
      * Walks to the destination via web walking.
@@ -158,16 +82,6 @@ public class Walking07 {
                 return compareGameDestination(tile);
             }
         }, 500);
-    }
-
-    /**
-     * Walks to the nearest bank via {@link Bank07#walkTo()}.
-     * <p>
-     * @see Bank07#walkTo()
-     * @return True if we reached a bank, false otherwise.
-     */
-    public static boolean walkToBank07() {
-        return Bank07.walkTo();
     }
 
     /**
@@ -198,28 +112,6 @@ public class Walking07 {
         return tile.equals(player_position);
     }
 
-    /**
-     * Executes the specified of ActionSets in order.
-     * <p>
-     * @param actionSets The ActionSets.
-     * @return True if the ActionSets were successfully executed, false otherwise.
-     */
-    public static boolean executeActionSets(ActionSet... actionSets) {
-        for (ActionSet actionSet : actionSets) {
-            if (!actionSet.execute()) {
-                return false;
-            }
-        }
-        return true;
-    }
 
-    /**
-     * Executes the specified of ActionSets in order.
-     * <p>
-     * @param actionSets The ActionSets.
-     * @return True if the ActionSets were successfully executed, false otherwise.
-     */
-    public static boolean executeActionSets(List<ActionSet> actionSets) {
-        return executeActionSets(actionSets.toArray(new ActionSet[actionSets.size()]));
-    }
+   
 }
